2cb50a1e0e24abc48e021fc124da85b0a921e045
Molk <33w22t@gmail.com>
2023-12-13 19:07:02 +0000

    feat: resolve panic

--- utils/log_util.go
+++ utils/log_util.go
@@ -15,8 +15,6 @@ func Error(str interface{}) {
 
 func RecoverHandler() {
 	if r := recover(); r != nil {
-		// 发生了 panic，r 包含了 panic 的信息
 		Error(fmt.Sprintf("Recovered from panic: %s", r))
-		// 可以在这里进行一些处理，如记录日志等
 	}
 }
